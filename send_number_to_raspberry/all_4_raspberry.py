614b7257827d0bc93cdf24d3baac7bd8e3da99fd
Arseniyyy <arskosh05@mail.ru>
2021-02-10 12:47:16 +0700

    Delete all_4_raspberry.py

--- send_number_to_raspberry/all_4_raspberry.py
+++
@@ -1,9 +0,0 @@
-# "All" file for raspberry. This file contain Server and Publisher instances, which works in infinite loop. Files executes consistently in the loop
-
-
-from publisher_camera_4_raspberry_1_1 import Publisher
-from server_echo_4_raspberry_1_1 import Server
-
-
-if __name__ == '__main__':
-    pass
